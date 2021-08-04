e879ff54564de92324b3c01ebc4000ef9ad7d443
Nycolas Messias <nycolas.messias@alest.com.br>
2021-08-04 00:09:06 -0300

    Docker files & Postgres config

--- config.js
+++ config.js
@@ -3,11 +3,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const {
-	DB_URL,
 	MONGO_URI,
+	DB_URL,
+	PG_USER,
+	PG_PASSWORD,
+	PG_HOST,
+	PG_PORT
 } = process.env;
 
 export {
-	DB_URL as connectionString,
 	MONGO_URI as mongoConnString,
+	DB_URL as pgConnString,
+	PG_USER as pgUser,
+	PG_PASSWORD as pgPass,
+	PG_HOST as pgHost,
+	PG_PORT as pgPort
 };
--- repositories/db.js
+++ repositories/db.js
@@ -1,14 +1,17 @@
-import {connectionString} from '../config.js';
+import { pgHost, pgPort, pgUser, pgPass } from '../config.js';
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-	connectionString,
-	{
+const sequelize = new Sequelize({
 		dialect: 'postgres',
+		host: pgHost,
+		port: pgPort,
+		database: 'db_videos',
+		username: pgUser,
+		password: pgPass,
+		logging: false,
 		define: {
 			timestamps: false,
-		},
-	},
-);
+		}
+	});
 
 export default sequelize;
